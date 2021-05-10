d711a9cb5354504416a74220c8eab59a71f1d971
11philip22 <philipwoldhek@gmail.com>
2021-05-10 13:51:36 +0200

    clean

--- ShellcodeA/ShellcodeA.c
+++ ShellcodeA/ShellcodeA.c
@@ -36,12 +36,12 @@ VOID Run()
 	UNICODE_STRING uString = { 0 };
 	STRING aString = { 0 };
 
-	WCHAR sUser32[] = { 'u', 's' ,'e' ,'r' ,'3' ,'2' ,'.' ,'d' ,'l' ,'l' };
+	WCHAR sUser32[] = { 'u', 's', 'e', 'r', '3', '2', '.', 'd', 'l', 'l' };
 
-	BYTE sMessageBoxW[] = { 'M','e','s','s','a','g','e','B','o','x','W', 0 };
+	BYTE sMessageBoxW[] = { 'M', 'e', 's', 's', 'a', 'g', 'e', 'B', 'o', 'x', 'W', 0 };
 
-	WCHAR sMsgContent[] = { 'H','e','l','l','o', ' ', 'W','o','r','l','d','!', 0 };
-	WCHAR sMsgTitle[] = { 'D','e','m','o','!', 0 };
+	WCHAR sMsgContent[] = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', 0 };
+	WCHAR sMsgTitle[] = { 'D', 'e', 'm', 'o', '!', 0 };
 
 	///
 	// STEP 1: locate all the required functions
